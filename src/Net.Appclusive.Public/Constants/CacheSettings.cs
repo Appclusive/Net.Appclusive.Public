86f381161ea762e636820d11198b9531538193e7
rufer7 <marc.rufer@d-fens.net>
2017-03-20 13:02:39 +0100

    CacheSettings
    * Added timeout for directory services credential validator

--- src/Net.Appclusive.Public/Constants/CacheSettings.cs
+++ src/Net.Appclusive.Public/Constants/CacheSettings.cs
@@ -33,6 +33,7 @@ namespace Net.Appclusive.Public.Constants
         public const long roleCacheManagerSettingsTimeout = 5 * DEFAULT_TIMEOUT;
         public const long rightCacheManagerSettingsTimeout = 5 * DEFAULT_TIMEOUT;
         public const long workflowCacheManagerSettingsTimeout = 30 * 60;
+        public const long directoryServicesCredentialValidatorCacheManagerSettingsTimeout = 24 * 60 * 60;
         // ReSharper restore InconsistentNaming
     }
 }
